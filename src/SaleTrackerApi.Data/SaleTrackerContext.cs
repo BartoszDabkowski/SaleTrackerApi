a62dad3b487cde2d06f982b4e13a62dd770a581b
bdabkows <Bartosz.Dabkowski@Pinkerton.com>
2017-02-09 11:31:49 -0800

    Added connection string to Db.

--- src/SaleTrackerApi.Data/SaleTrackerContext.cs
+++ src/SaleTrackerApi.Data/SaleTrackerContext.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using SaleTrackerApi.Data.Entities;
 
 namespace SaleTrackerApi.Data
 {
     public class SaleTrackerContext : DbContext
     {
-        public SaleTrackerContext()
+        private readonly IConfigurationRoot _config;
+
+        public SaleTrackerContext(IConfigurationRoot config, DbContextOptions options)
+            :base(options)
         {
+            _config = config;
         }
 
         public DbSet<Store> Stores { get; set; }
         public DbSet<Sale> Sales { get; set; }
         public DbSet<SalesLineItem> SalesLineItems { get; set; }
         public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_config[""]);
+        }
     }
 }
