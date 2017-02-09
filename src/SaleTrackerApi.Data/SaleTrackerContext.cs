1db0668ab86be139fac27a6c2530eab04e735010
bdabkows <Bartosz.Dabkowski@Pinkerton.com>
2017-02-09 11:09:25 -0800

    Added SaleTrackerContext.

---
+++ src/SaleTrackerApi.Data/SaleTrackerContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SaleTrackerApi.Data.Entities;
+
+namespace SaleTrackerApi.Data
+{
+    public class SaleTrackerContext : DbContext
+    {
+        public SaleTrackerContext()
+        {
+        }
+
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<SalesLineItem> SalesLineItems { get; set; }
+        public DbSet<Item> Items { get; set; }
+    }
+}
